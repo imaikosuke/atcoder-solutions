d875ae1c76c4dd119e878d2b7b02244b646c02de
imaikosuke <imaikosuke2@gmail.com>
2024-02-16 23:45:11 +0900

    add comment ABC249 - B

--- ABC249/b_perfect_string.py
+++ ABC249/b_perfect_string.py
@@ -1,8 +1,8 @@
 s = input()
-if len(s) != len(set(s)):
+if len(s) != len(set(s)): # 重複がある場合
     print("No")
     exit()
-if s.islower() or s.isupper():
+if s.islower() or s.isupper(): # すべて小文字か大文字の場合
     print("No")
     exit()
 print("Yes")
